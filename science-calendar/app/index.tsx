e6eecd064baa24dba107e5d16f804e70cf160505
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2026-02-24 19:54:04 +0700

    feat: enhance layout and update index component with current date and facts

--- science-calendar/app/index.tsx
+++ science-calendar/app/index.tsx
@@ -1,18 +1,43 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+const currentDate: Date = new Date();
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "black",
-      }}
-    >
-      <Text style={{ color: "white", fontSize: 30, fontWeight: "bold" }}>
-        Hello World
-      </Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.monthYearText}>
+          Tháng {currentDate.getMonth() + 1} năm {currentDate.getFullYear()}
+        </Text>
+        <Text style={styles.dayText}>{currentDate.getDate()}</Text>
+        <Text style={styles.factText}>
+          Nguyên tử chỉ là các không gian trống
+        </Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  monthYearText: {
+    fontSize: 24,
+    color: "#fff",
+  },
+  dayText: {
+    fontSize: 72,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  factText: {
+    fontSize: 18,
+    color: "#fff",
+    position: "absolute",
+    bottom: 80,
+  },
+});
