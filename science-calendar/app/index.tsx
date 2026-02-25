cfd2ce22d635a5b220d0edeaa3b8353fc9200128
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2026-02-25 20:05:43 +0700

    feat: add Icon component to enhance the visual layout of the index page

--- science-calendar/app/index.tsx
+++ science-calendar/app/index.tsx
@@ -1,21 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 const currentDate: Date = new Date();
 
 export default function Index() {
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.monthYearText}>
-          Tháng {currentDate.getMonth() + 1} năm {currentDate.getFullYear()}
-        </Text>
-        <Text style={styles.dayText}>{currentDate.getDate()}</Text>
-        <Link href="/fact" style={styles.factText}>
-          <Text>Nguyên tử chỉ là các không gian trống</Text>
-        </Link>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Icon
+        name="account-circle"
+        size={30}
+        color="#fff"
+        style={{ position: "absolute", top: 50, right: 20 }}
+      />
+      <Text style={styles.monthYearText}>
+        Tháng {currentDate.getMonth() + 1} năm {currentDate.getFullYear()}
+      </Text>
+      <Text style={styles.dayText}>{currentDate.getDate()}</Text>
+      <Link href="/fact" style={styles.factText}>
+        <Text>Nguyên tử chỉ là các không gian trống</Text>
+      </Link>
+    </View>
   );
 }
 
