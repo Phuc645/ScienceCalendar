2ce5b3fadd727d92b147a1eacca1ecdcb2616467
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2026-02-27 19:40:33 +0700

    feat: update Account component with login prompt and button for enhanced user experience

--- science-calendar/app/account.tsx
+++ science-calendar/app/account.tsx
@@ -1,8 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "@react-native-material/core";
+
 export default function Account() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>Account</Text>
+      <Text style={styles.titleText}>
+        Hãy đăng nhập để có trải nghiệm tốt nhất
+      </Text>
+      <Button title="Đăng nhập" variant="contained" color="#fff" />
     </View>
   );
 }
@@ -14,11 +19,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#25292e",
     paddingTop: 100,
+    gap: 50,
   },
   titleText: {
     fontSize: 36,
-    fontWeight: "bold",
     color: "#fff",
     marginHorizontal: 20,
+    textAlign: "center",
   },
 });
