89e095ad8d3e088f82ec47b961a5e0a13d56ec64
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2026-03-04 19:53:31 +0700

    Update the account component logic

--- science-calendar/app/account.tsx
+++ science-calendar/app/account.tsx
@@ -2,21 +2,31 @@ import { StyleSheet, Text, View } from "react-native";
 import { Button } from "@react-native-material/core";
 import { router } from "expo-router";
 
+const isSignedUp: boolean = true;
+
 export default function Account() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>
-        Hãy đăng nhập để có trải nghiệm tốt nhất
-      </Text>
-      <Button
-        title="Đăng nhập"
-        variant="contained"
-        color="#fff"
-        tintColor="#000"
-        onPress={() => router.push("/login")}
-      />
-    </View>
-  );
+  if (!isSignedUp) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Hãy đăng nhập để có trải nghiệm tốt nhất
+        </Text>
+        <Button
+          title="Đăng nhập"
+          variant="contained"
+          color="#fff"
+          tintColor="#000"
+          onPress={() => router.push("/login")}
+        />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Account</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
