5b826fec0ab3294f03a52f3628a851cd505a8f83
Tu Hoang Gia Phuc <tuhoanggiap@gmail.com>
2026-03-09 19:18:26 +0700

    feat: add Firebase dependency and update package-lock.json with related Firebase modules

---
+++ science-calendar/firebaseConfig.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDI1NXa6LY06ZGzHpd26KluAqBsNz2IQM",
+  authDomain: "science-calendar-f8260.firebaseapp.com",
+  projectId: "science-calendar-f8260",
+  storageBucket: "science-calendar-f8260.firebasestorage.app",
+  messagingSenderId: "568083807934",
+  appId: "1:568083807934:web:ee98a9656b435542ce28ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
